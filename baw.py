83db2a90f9bd5ef40499c43b1155ef027148f733
Manuel Cabral <cabral.manuel@yandex.com>
2021-03-23 00:07:21 -0300

    modify pixels

---
+++ baw.py
@@ -0,0 +1,18 @@
+import cv2
+
+image = cv2.imread('duck.jpg', -1)
+shape = image.shape # height, width, channels -> 3 {red, green, blue}
+height, width, channels = shape[0], shape[1], shape[2]
+
+for row in range(1, height - 1):
+    for column in range(1, width - 1):
+
+        red, green, blue = image[row][column][0], image[row][column][1], image[row][column][2]
+        gray = int(red) + int(green) + int(blue) / 3
+
+        for i in range(channels): 
+            image[row][column][i] = gray
+
+cv2.imshow('Black and white', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
